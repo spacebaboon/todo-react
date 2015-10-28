6df17358d65f8e2edf0e368659c3599cf937d991
Ben Avery <spacebaboon@gmail.com>
2015-10-28 17:55:06 +0000

    add test infrastructure and initial editable tests.

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,34 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
+var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pkg = require('./package.json');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
-console.log('root path is %s', ROOT_PATH);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var TEST_PATH = path.resolve(ROOT_PATH, 'test');
+
+process.env.BABEL_ENV = TARGET;
 
 var common = {
-    entry: path.resolve(ROOT_PATH, 'app'),
+    entry: APP_PATH,
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     output: {
-        path: path.resolve(ROOT_PATH, 'build'),
+        path: BUILD_PATH,
         filename: 'bundle.js'
     },
     module: {
         loaders: [
             {
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: path.resolve(ROOT_PATH, 'app')
+                test: /\.jsx?$/,
+                loaders: ['babel'],
+                include: APP_PATH
             }
         ]
     },
@@ -32,9 +40,18 @@ var common = {
     ]
 };
 
-if (TARGET === 'start' || !TARGET) {
+if(TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
         devtool: 'eval-source-map',
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loaders: ['style', 'css'],
+                    include: APP_PATH
+                }
+            ]
+        },
         devServer: {
             historyApiFallback: true,
             hot: true,
@@ -47,16 +64,71 @@ if (TARGET === 'start' || !TARGET) {
     });
 }
 
-
-if (TARGET === 'start' || !TARGET) {
+if(TARGET === 'build' || TARGET === 'stats') {
     module.exports = merge(common, {
-        devtool: 'eval-source-map',
+        entry: {
+            app: APP_PATH,
+            vendor: Object.keys(pkg.dependencies)
+        },
+        output: {
+            path: BUILD_PATH,
+            filename: '[name].[chunkhash].js'
+        },
+        devtool: 'source-map',
         module: {
             loaders: [
                 {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css'),
+                    include: APP_PATH
+                }
+            ]
+        },
+        plugins: [
+            new Clean(['build']),
+            new ExtractTextPlugin('styles.[chunkhash].css'),
+            new webpack.optimize.CommonsChunkPlugin(
+                'vendor',
+                '[name].[chunkhash].js'
+            ),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    // This affects react lib size
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+    });
+}
+
+if(TARGET === 'test' || TARGET === 'tdd') {
+    module.exports = merge(common, {
+        entry: {},
+        output: {},
+        devtool: 'inline-source-map',
+        resolve: {
+            alias: {
+                'app': APP_PATH
+            }
+        },
+        module: {
+            preLoaders: [
+                {
                     test: /\.jsx?$/,
-                    loaders: ['react-hot', 'babel'],
-                    include: path.resolve(ROOT_PATH, 'app')
+                    loaders: ['isparta-instrumenter'],
+                    include: APP_PATH
+                }
+            ],
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loaders: ['babel'],
+                    include: TEST_PATH
                 }
             ]
         }
